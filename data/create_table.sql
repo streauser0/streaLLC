5ab2edd070ffdb61c3966fca2eacac251cb65c71
Spencer Nichols <spencerwnichols@msn.com>
2023-01-18 18:59:20 -0700

    The mysql stuff

--- data/create_table.sql
+++ data/create_table.sql
@@ -1,14 +1,25 @@
 use strea;
+DROP TABLE IF EXISTS strea.Summary;
+DROP TABLE IF EXISTS strea.Company;
 
 CREATE TABLE Company (
-    Id int NOT NULL,
     Symbol varchar(255) NOT NULL,
+    Name varchar(255),
     Description varchar(255),
-    PRIMARY KEY (Id)
+    PRIMARY KEY (Symbol)
 );
 
 CREATE TABLE Summary (
     Id int NOT NULL,
     Symbol varchar(255) NOT NULL,
-    PRIMARY KEY (Id)
+    FiftyTwoWeekHigh DECIMAL(8, 2),
+	FiftyTwoWeekLow DECIMAL(8, 2),
+    PE decimal(8,2),
+    PRIMARY KEY (Id),
+	FOREIGN KEY (symbol) REFERENCES Company(Symbol)
+     
 );
+
+select * from Summary;
+
+
--- data/insert_company_data.sql
+++ data/insert_company_data.sql
@@ -1,2 +1,11 @@
-INSERT INTO company (Id, Symbol, Description)   
-VALUES (0, 'CVX', 'Chevron');
+-- Default data for company
+INSERT INTO company (Symbol, name, Description)   
+VALUES ('CVX', 'Chevron', 'The Chevron Corp');
+
+-- Default data for summary
+INSERT INTO Summary (Id, Symbol, FiftyTwoWeekHigh, FiftytwoWeekLow, PE)   
+VALUES (0, 'CVX', 189.68, 122.84, 10.08);
+INSERT INTO Summary (Id, Symbol, FiftyTwoWeekHigh, FiftytwoWeekLow, PE)   
+VALUES (1, 'CVX', 190.68, 121.84, 12.08);
+INSERT INTO Summary (Id, Symbol, FiftyTwoWeekHigh, FiftytwoWeekLow, PE)   
+VALUES (2, 'CVX', 200.68, 125.84, 2.08);
